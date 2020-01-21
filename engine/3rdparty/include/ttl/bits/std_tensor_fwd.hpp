d103ac628afc657c315bb0b07af99a164b9c72b3
lg <lg4869@outlook.com>
2020-01-21 23:45:07 +0000

    <ttl/experimental/low_rank_tensor>

---
+++ engine/3rdparty/include/ttl/bits/std_tensor_fwd.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+namespace ttl
+{
+namespace internal
+{
+struct owner;
+struct readwrite;
+struct readonly;
+
+template <typename R, typename D>
+class basic_allocator;
+
+template <typename R, typename D>
+class basic_deallocator;
+
+template <typename D1, typename D2>
+class basic_copier;
+
+template <typename R, typename A, typename D>
+struct basic_tensor_traits;
+
+template <typename R, typename S, typename D, typename A>
+class basic_scalar_mixin;
+
+template <typename R, typename S, typename D, typename A>
+class basic_tensor_mixin;
+
+template <typename R, typename S, typename D, typename A>
+class basic_tensor;
+
+template <typename Encoder, typename S, typename D, typename A>
+class basic_raw_tensor;
+}  // namespace internal
+}  // namespace ttl
